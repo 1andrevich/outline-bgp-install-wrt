9e4d794987420f95c31d0912ff4a1710d47bf8f0
Andrevich <47223721+1andrevich@users.noreply.github.com>
2025-09-01 17:13:56 +0400

    Update install_outline.sh
    Updated for v.2.6.0 (warn instead of warning log level)

--- install_outline.sh
+++ install_outline.sh
@@ -151,14 +151,14 @@ STOP=89
 #PROG=/usr/bin/tun2socks
 #IF="tun1"
 #OUTLINE_CONFIG="$OUTLINECONF"
-#LOGLEVEL="warning"
+#LOGLEVEL="warn"
 #BUFFER="64kb"
 
 #Check for tun2socks then download tun2socks binary from GitHub
 start_service() {
     procd_open_instance
     procd_set_param user root
-    procd_set_param command /usr/bin/tun2socks -device tun1 -tcp-rcvbuf 64kb -tcp-sndbuf 64kb  -proxy "$OUTLINECONF" -loglevel "warning"
+    procd_set_param command /usr/bin/tun2socks -device tun1 -tcp-rcvbuf 64kb -tcp-sndbuf 64kb  -proxy "$OUTLINECONF" -loglevel "warn"
     procd_set_param stdout 1
     procd_set_param stderr 1
     procd_set_param respawn \${respawn_threshold:-3600} \${respawn_timeout:-5} \${respawn_retry:-5}
